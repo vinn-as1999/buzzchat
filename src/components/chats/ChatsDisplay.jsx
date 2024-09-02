fdcce24beb9d56709a4861edc748506cd1b6cdc8
Vinicyus <vinicyus.alves302@gmail.com>
2024-09-02 08:27:01 -0300

    new style changes

--- src/components/chats/ChatsDisplay.jsx
+++ src/components/chats/ChatsDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import ChatBttn from './ChatBttn.jsx';
 import NoFriends from '../NoFriends.jsx';
+import { FaRocketchat } from 'react-icons/fa';
 
 const ChatsDisplay = (props) => {
   const friendsArray = localStorage.getItem('friends');
@@ -9,6 +10,12 @@ const ChatsDisplay = (props) => {
   return (
     <>
       <section className='chats'>
+        <div className='homePageTitle'>
+          <div style={{padding: '0px 10px'}}>
+            <span>Buzz</span>
+            <span style={{color: '#A537C4'}}>Chat</span>
+          </div><FaRocketchat color= '#A537C4' />
+        </div>
         <input style={{width: '34vw', borderColor: 'grey'}} type="text" 
         placeholder='Search for chats' autoFocus="true" /> {/* it doesn't works for a while*/}
         {
